0cfb2a7d7eb8a1986b69c04cf34b1d0aa71b4ba1
Matheus Santos <matheus.santos@fengbrasil.com.br>
2026-09-16 23:04:25 -0300

    feature: implements bio

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/bio",
+        destination: "/bio/index.html",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
+
